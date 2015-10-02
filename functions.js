b865c6723d696bfa3757d51f6c6adc6f60dd0b6e
EstiBlancoElorrieta <EstiBlancoElorrieta@users.noreply.github.com>
2015-10-01 20:03:12 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -15,3 +15,12 @@ function tabSwitch(new_tab, new_content) {
     document.getElementById(new_tab).className = 'active';      
  
 }
+var url = document.location.toString();
+if (url.match('#')) {
+    $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+} 
+
+// Change hash for page-reload
+$('.nav-tabs a').on('shown.bs.tab', function (e) {
+    window.location.hash = e.target.hash;
+})
